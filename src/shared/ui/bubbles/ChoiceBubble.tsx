6756b1b2fc5ed9f10ca2255db2f8890c3487f596
marcsek <jakubmarcek955@gmail.com>
2026-09-06 14:42:23 +0200

    preserve game controls hover state on click

--- src/shared/ui/bubbles/ChoiceBubble.tsx
+++ src/shared/ui/bubbles/ChoiceBubble.tsx
@@ -19,8 +19,8 @@ export default function ChoiceBubbles({ bubbles }: ChoiceBubblesProps) {
       {bubbles.map(({ value, latex = true, onClick }, idx) => {
         return (
           <Button
-            // If number of buttons changes, don't preserve
-            key={idx + 2 * bubbles.length}
+            // Keying by index preserves hover state
+            key={idx}
             size="sm"
             variant="outline-primary d-inline m-1"
             onClick={onClick}
